f04b549b3979442d33d94beb7a15d3e5efa94ea1
Nikhil MV <[nikhilvenu2002@gmail.com]>
2024-10-07 09:44:56 +0530

    second commit

--- csharp-project/csharp-project/Program.cs
+++ csharp-project/csharp-project/Program.cs
@@ -10,6 +10,8 @@ namespace csharp_project
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
         }
     }
 }
